f01bc9daf84ec04e418a6bc6a93be026c60a1a12
bomkei <bomkei@abcdef.ghi>
2022-06-02 22:59:55 +0900

    add c_cpp_properties.json

---
+++ Build/cheats.d
@@ -0,0 +1,277 @@
+cheats.o: C:/Users/mrzkr/AnimalBytes/Sources/cheats.cpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/cheats.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/Color.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/types.h \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/OSD.hpp \
+ C:/devkitPro/libctru/include/3ds.h \
+ C:/devkitPro/libctru/include/3ds/types.h \
+ C:/devkitPro/libctru/include/3ds/result.h \
+ C:/devkitPro/libctru/include/3ds/ipc.h \
+ C:/devkitPro/libctru/include/3ds/svc.h \
+ C:/devkitPro/libctru/include/3ds/exheader.h \
+ C:/devkitPro/libctru/include/3ds/srv.h \
+ C:/devkitPro/libctru/include/3ds/errf.h \
+ C:/devkitPro/libctru/include/3ds/os.h \
+ C:/devkitPro/libctru/include/3ds/synchronization.h \
+ C:/devkitPro/libctru/include/3ds/thread.h \
+ C:/devkitPro/libctru/include/3ds/gfx.h \
+ C:/devkitPro/libctru/include/3ds/services/gspgpu.h \
+ C:/devkitPro/libctru/include/3ds/console.h \
+ C:/devkitPro/libctru/include/3ds/env.h \
+ C:/devkitPro/libctru/include/3ds/util/decompress.h \
+ C:/devkitPro/libctru/include/3ds/util/utf.h \
+ C:/devkitPro/libctru/include/3ds/allocator/linear.h \
+ C:/devkitPro/libctru/include/3ds/allocator/mappable.h \
+ C:/devkitPro/libctru/include/3ds/allocator/vram.h \
+ C:/devkitPro/libctru/include/3ds/services/ac.h \
+ C:/devkitPro/libctru/include/3ds/services/am.h \
+ C:/devkitPro/libctru/include/3ds/services/fs.h \
+ C:/devkitPro/libctru/include/3ds/services/ampxi.h \
+ C:/devkitPro/libctru/include/3ds/services/apt.h \
+ C:/devkitPro/libctru/include/3ds/services/boss.h \
+ C:/devkitPro/libctru/include/3ds/services/cam.h \
+ C:/devkitPro/libctru/include/3ds/services/y2r.h \
+ C:/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ C:/devkitPro/libctru/include/3ds/services/cfgu.h \
+ C:/devkitPro/libctru/include/3ds/services/csnd.h \
+ C:/devkitPro/libctru/include/3ds/services/dsp.h \
+ C:/devkitPro/libctru/include/3ds/services/fspxi.h \
+ C:/devkitPro/libctru/include/3ds/services/fsreg.h \
+ C:/devkitPro/libctru/include/3ds/services/frd.h \
+ C:/devkitPro/libctru/include/3ds/mii.h \
+ C:/devkitPro/libctru/include/3ds/services/gsplcd.h \
+ C:/devkitPro/libctru/include/3ds/services/hid.h \
+ C:/devkitPro/libctru/include/3ds/services/irrst.h \
+ C:/devkitPro/libctru/include/3ds/services/sslc.h \
+ C:/devkitPro/libctru/include/3ds/services/httpc.h \
+ C:/devkitPro/libctru/include/3ds/services/uds.h \
+ C:/devkitPro/libctru/include/3ds/services/ndm.h \
+ C:/devkitPro/libctru/include/3ds/services/nim.h \
+ C:/devkitPro/libctru/include/3ds/services/nwmext.h \
+ C:/devkitPro/libctru/include/3ds/services/ir.h \
+ C:/devkitPro/libctru/include/3ds/services/ns.h \
+ C:/devkitPro/libctru/include/3ds/services/pmapp.h \
+ C:/devkitPro/libctru/include/3ds/services/pmdbg.h \
+ C:/devkitPro/libctru/include/3ds/services/ps.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmu.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmsysm.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmgets.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmsets.h \
+ C:/devkitPro/libctru/include/3ds/services/pxidev.h \
+ C:/devkitPro/libctru/include/3ds/services/pxipm.h \
+ C:/devkitPro/libctru/include/3ds/services/soc.h \
+ C:/devkitPro/libctru/include/netinet/in.h \
+ C:/devkitPro/libctru/include/sys/socket.h \
+ C:/devkitPro/libctru/include/3ds/services/mic.h \
+ C:/devkitPro/libctru/include/3ds/services/mvd.h \
+ C:/devkitPro/libctru/include/3ds/services/nfc.h \
+ C:/devkitPro/libctru/include/3ds/services/news.h \
+ C:/devkitPro/libctru/include/3ds/services/qtm.h \
+ C:/devkitPro/libctru/include/3ds/services/srvpm.h \
+ C:/devkitPro/libctru/include/3ds/services/loader.h \
+ C:/devkitPro/libctru/include/3ds/services/mcuhwc.h \
+ C:/devkitPro/libctru/include/3ds/gpu/gx.h \
+ C:/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ C:/devkitPro/libctru/include/3ds/gpu/registers.h \
+ C:/devkitPro/libctru/include/3ds/gpu/enums.h \
+ C:/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ C:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ C:/devkitPro/libctru/include/3ds/ndsp/ndsp.h \
+ C:/devkitPro/libctru/include/3ds/ndsp/channel.h \
+ C:/devkitPro/libctru/include/3ds/applets/swkbd.h \
+ C:/devkitPro/libctru/include/3ds/applets/error.h \
+ C:/devkitPro/libctru/include/3ds/applets/miiselector.h \
+ C:/devkitPro/libctru/include/3ds/archive.h \
+ C:/devkitPro/libctru/include/3ds/romfs.h \
+ C:/devkitPro/libctru/include/3ds/font.h \
+ C:/devkitPro/libctru/include/3ds/gdbhio_dev.h \
+ C:/devkitPro/libctru/include/3ds/3dslink.h \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/Render.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/Keyboard.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/CustomIcon.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Controller.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Vector.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound/Sound.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound/SoundEngine.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntry.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntryHotkeys.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntryHotkeys.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuFolder.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/PluginMenu.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Time.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MessageBox.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Clock.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Directory.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/File.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Mutex.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/FwkSettings.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Rect.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Vector.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Hook.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Lock.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Process.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/System.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Task.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Thread.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Touch.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Sleep.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/LineReader.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/LineWriter.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/StringExtensions.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/Utils.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/AutoRegion.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/HoldKey.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/KeySequence.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/MenuEntryHelpers.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/OSDManager.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/QuickMenu.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/HoldKey.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/Strings.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Helpers/Wrappers.hpp \
+ C:/Users/mrzkr/AnimalBytes/Includes/Unicode.h
+C:/Users/mrzkr/AnimalBytes/Includes/cheats.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/Color.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/types.h:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/OSD.hpp:
+C:/devkitPro/libctru/include/3ds.h:
+C:/devkitPro/libctru/include/3ds/types.h:
+C:/devkitPro/libctru/include/3ds/result.h:
+C:/devkitPro/libctru/include/3ds/ipc.h:
+C:/devkitPro/libctru/include/3ds/svc.h:
+C:/devkitPro/libctru/include/3ds/exheader.h:
+C:/devkitPro/libctru/include/3ds/srv.h:
+C:/devkitPro/libctru/include/3ds/errf.h:
+C:/devkitPro/libctru/include/3ds/os.h:
+C:/devkitPro/libctru/include/3ds/synchronization.h:
+C:/devkitPro/libctru/include/3ds/thread.h:
+C:/devkitPro/libctru/include/3ds/gfx.h:
+C:/devkitPro/libctru/include/3ds/services/gspgpu.h:
+C:/devkitPro/libctru/include/3ds/console.h:
+C:/devkitPro/libctru/include/3ds/env.h:
+C:/devkitPro/libctru/include/3ds/util/decompress.h:
+C:/devkitPro/libctru/include/3ds/util/utf.h:
+C:/devkitPro/libctru/include/3ds/allocator/linear.h:
+C:/devkitPro/libctru/include/3ds/allocator/mappable.h:
+C:/devkitPro/libctru/include/3ds/allocator/vram.h:
+C:/devkitPro/libctru/include/3ds/services/ac.h:
+C:/devkitPro/libctru/include/3ds/services/am.h:
+C:/devkitPro/libctru/include/3ds/services/fs.h:
+C:/devkitPro/libctru/include/3ds/services/ampxi.h:
+C:/devkitPro/libctru/include/3ds/services/apt.h:
+C:/devkitPro/libctru/include/3ds/services/boss.h:
+C:/devkitPro/libctru/include/3ds/services/cam.h:
+C:/devkitPro/libctru/include/3ds/services/y2r.h:
+C:/devkitPro/libctru/include/3ds/services/cfgnor.h:
+C:/devkitPro/libctru/include/3ds/services/cfgu.h:
+C:/devkitPro/libctru/include/3ds/services/csnd.h:
+C:/devkitPro/libctru/include/3ds/services/dsp.h:
+C:/devkitPro/libctru/include/3ds/services/fspxi.h:
+C:/devkitPro/libctru/include/3ds/services/fsreg.h:
+C:/devkitPro/libctru/include/3ds/services/frd.h:
+C:/devkitPro/libctru/include/3ds/mii.h:
+C:/devkitPro/libctru/include/3ds/services/gsplcd.h:
+C:/devkitPro/libctru/include/3ds/services/hid.h:
+C:/devkitPro/libctru/include/3ds/services/irrst.h:
+C:/devkitPro/libctru/include/3ds/services/sslc.h:
+C:/devkitPro/libctru/include/3ds/services/httpc.h:
+C:/devkitPro/libctru/include/3ds/services/uds.h:
+C:/devkitPro/libctru/include/3ds/services/ndm.h:
+C:/devkitPro/libctru/include/3ds/services/nim.h:
+C:/devkitPro/libctru/include/3ds/services/nwmext.h:
+C:/devkitPro/libctru/include/3ds/services/ir.h:
+C:/devkitPro/libctru/include/3ds/services/ns.h:
+C:/devkitPro/libctru/include/3ds/services/pmapp.h:
+C:/devkitPro/libctru/include/3ds/services/pmdbg.h:
+C:/devkitPro/libctru/include/3ds/services/ps.h:
+C:/devkitPro/libctru/include/3ds/services/ptmu.h:
+C:/devkitPro/libctru/include/3ds/services/ptmsysm.h:
+C:/devkitPro/libctru/include/3ds/services/ptmgets.h:
+C:/devkitPro/libctru/include/3ds/services/ptmsets.h:
+C:/devkitPro/libctru/include/3ds/services/pxidev.h:
+C:/devkitPro/libctru/include/3ds/services/pxipm.h:
+C:/devkitPro/libctru/include/3ds/services/soc.h:
+C:/devkitPro/libctru/include/netinet/in.h:
+C:/devkitPro/libctru/include/sys/socket.h:
+C:/devkitPro/libctru/include/3ds/services/mic.h:
+C:/devkitPro/libctru/include/3ds/services/mvd.h:
+C:/devkitPro/libctru/include/3ds/services/nfc.h:
+C:/devkitPro/libctru/include/3ds/services/news.h:
+C:/devkitPro/libctru/include/3ds/services/qtm.h:
+C:/devkitPro/libctru/include/3ds/services/srvpm.h:
+C:/devkitPro/libctru/include/3ds/services/loader.h:
+C:/devkitPro/libctru/include/3ds/services/mcuhwc.h:
+C:/devkitPro/libctru/include/3ds/gpu/gx.h:
+C:/devkitPro/libctru/include/3ds/gpu/gpu.h:
+C:/devkitPro/libctru/include/3ds/gpu/registers.h:
+C:/devkitPro/libctru/include/3ds/gpu/enums.h:
+C:/devkitPro/libctru/include/3ds/gpu/shbin.h:
+C:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+C:/devkitPro/libctru/include/3ds/ndsp/ndsp.h:
+C:/devkitPro/libctru/include/3ds/ndsp/channel.h:
+C:/devkitPro/libctru/include/3ds/applets/swkbd.h:
+C:/devkitPro/libctru/include/3ds/applets/error.h:
+C:/devkitPro/libctru/include/3ds/applets/miiselector.h:
+C:/devkitPro/libctru/include/3ds/archive.h:
+C:/devkitPro/libctru/include/3ds/romfs.h:
+C:/devkitPro/libctru/include/3ds/font.h:
+C:/devkitPro/libctru/include/3ds/gdbhio_dev.h:
+C:/devkitPro/libctru/include/3ds/3dslink.h:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/Render.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/Keyboard.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Graphics/CustomIcon.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Controller.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Vector.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound/Sound.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound/SoundEngine.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntry.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntryHotkeys.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuEntryHotkeys.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MenuFolder.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/PluginMenu.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Time.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Menu/MessageBox.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Sound.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Clock.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Directory.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/File.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Mutex.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/FwkSettings.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Rect.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Vector.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Hook.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Lock.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Process.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/System.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Task.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Thread.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Touch.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/System/Sleep.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/LineReader.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/LineWriter.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/StringExtensions.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/CTRPluginFramework/Utils/Utils.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/AutoRegion.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/HoldKey.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/KeySequence.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/MenuEntryHelpers.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/OSDManager.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/QuickMenu.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/HoldKey.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/Strings.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Helpers/Wrappers.hpp:
+C:/Users/mrzkr/AnimalBytes/Includes/Unicode.h:
