244ac2641da3b63ee0fef743b22c28e6de80b528
yak9909 <Mayu8680@yahoo.co.jp>
2022-06-04 06:24:38 +0900

    fix PocketItem.cpp

--- Sources/Cheats/Items/PocketItem.cpp
+++ Sources/Cheats/Items/PocketItem.cpp
@@ -4,13 +4,16 @@
 
 namespace CTRPluginFramework::Cheats::Items {
   void pocket_item(MenuEntry* entry) {
+    u32 keys = Controller::GetKeysDown();
     Keyboard kbd("取得するアイテムIDを入力");
     u32 itemid;
 
-    int result = kbd.Open(itemid);
-    if( result >= 0 ) {
-      int inv_index = ACNL::Player::WriteItem(itemid);
-      OSD::Notify("Pocket Item: " << Color::Yellow << "Index " << Color::Green << std::to_string(inv_index));
+    if( Controller::IsKeysPressed(Key::L + Key::B) ) {
+      int result = kbd.Open(itemid);
+      if( result >= 0 ) {
+        int inv_index = ACNL::Player::WriteItem(itemid);
+        OSD::Notify("Pocket Item: " << Color::Yellow << "Index " << Color::Green << std::to_string(inv_index));
+      }
     }
   }
 }
