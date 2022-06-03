8157c9642208c3b67bd1a6470cc73d495caf7db2
yak9909 <Mayu8680@yahoo.co.jp>
2022-06-04 06:42:15 +0900

    fix Player.h

--- Includes/ACNL/Player.h
+++ Includes/ACNL/Player.h
@@ -3,19 +3,16 @@
 
 namespace ACNL {
   class Player {
+
   public:
     
     static int WriteItem(u32 item, u16 slot = -1) {
-      u32* inv_addrs = (u32*)((*(u32*)(0xAA914c) + 0x6BD0));
+      u32* player_pointer = (u32*)(0xAA914c);
+      u32* inv_addrs = (u32*)(player_pointer[0] + 0x6BD0);
 
       if( slot < 0 ) {
         for( int i = 0; i < 16; i++) {
-          if( inv_addrs[i] == 0x7FFE ) {
-            inv_addrs[i] = item;
-            return i;
-          }
-
-          if( i == 15 ) {
+          if( inv_addrs[i] == 0x7FFE || i == 15 ) {
             inv_addrs[i] = item;
             return i;
           }
@@ -27,7 +24,9 @@ namespace ACNL {
     }
 
     static u32 ReadItem(u16 slot) {
-      u32* inv_addrs = (u32*)((*(u32*)(0xAA914c) + 0x6BD0));
+      u32* player_pointer = (u32*)(0xAA914c);
+      u32* inv_addrs = (u32*)(player_pointer[0] + 0x6BD0);
+
       return inv_addrs[slot * 4];
     }
 
