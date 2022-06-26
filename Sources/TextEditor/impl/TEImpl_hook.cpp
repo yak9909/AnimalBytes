f7100f36b1b0cd5ff7c7899c13a4bed64e3609a9
bomkei <93323570+bomkei@users.noreply.github.com>
2022-06-26 13:14:52 +0900

    [TextEditor] r4-r7 を保存

--- Sources/TextEditor/impl/TEImpl_hook.cpp
+++ Sources/TextEditor/impl/TEImpl_hook.cpp
@@ -17,19 +17,25 @@ namespace CTRPluginFramework {
   static std::vector<std::pair<u32*, u32>> _hook_bak;
 
   void TextEditorImpl::_KbdImpl_RenderTop_hook(void*) {
+    asm volatile ("push {r4-r7}");
+
     auto& te = *TextEditor::get_instance()->impl;
     auto const& screen = OSD::GetTopScreen();
     
     te.draw(screen);
+
+    asm volatile ("pop {r4-r7}");
   }
 
   void TextEditorImpl::_KbdImpl_RenderBottom_hook(void*) {
+    asm volatile ("push {r4-r7}");
     
     auto& te = *TextEditor::get_instance()->impl;
     auto const& screen = OSD::GetBottomScreen();
 
     te.draw_bottom(screen);
 
+    asm volatile ("pop {r4-r7}");
   }
 
   void TextEditorImpl::_hook_init() {
@@ -40,11 +46,14 @@ namespace CTRPluginFramework {
       // Don't return with KEY_ENTER
       { (u32*)_ZN18CTRPluginFramework12KeyboardImpl3RunEv + 117, 0xEA00000B },
 
-      // Allow space key
+      // Space key
       { (u32*)_ZN18CTRPluginFramework12KeyboardImpl10_CheckKeysEv + 203,
           (0xEA << 24) | calc_branch_offset((u32)_ZN18CTRPluginFramework12KeyboardImpl10_CheckKeysEv + 0x32C,
             (u32)_ZN18CTRPluginFramework12KeyboardImpl10_CheckKeysEv + 0x380) },
       
+      // Backspace
+      { (u32*)_ZN18CTRPluginFramework12KeyboardImpl10_CheckKeysEv + 339, 0xE3550001 },
+
       // Draw editor
       { (u32*)_ZN18CTRPluginFramework12KeyboardImpl10_RenderTopEv, asmjmp },
       { (u32*)_ZN18CTRPluginFramework12KeyboardImpl10_RenderTopEv + 1, (u32)TextEditorImpl::_KbdImpl_RenderTop_hook },
@@ -54,8 +63,13 @@ namespace CTRPluginFramework {
       { (u32*)_ZN18CTRPluginFramework12KeyboardImpl13_RenderBottomEv + 135, (u32)_hookHelperFn_KbdImpl_RenderBottom },
     };
 
+    if( _hook_bak.empty() ) {
+      for( auto&& item : hookMap ) {
+        _hook_bak.emplace_back(item.first, *item.first);
+      }
+    }
+
     for( auto&& item : hookMap ) {
-      _hook_bak.emplace_back(item.first, *item.first);
       *item.first = item.second;
     }
   }
@@ -64,7 +78,5 @@ namespace CTRPluginFramework {
     for( auto&& item : _hook_bak ) {
       *item.first = item.second;
     }
-
-    _hook_bak.clear();
   }
 }
