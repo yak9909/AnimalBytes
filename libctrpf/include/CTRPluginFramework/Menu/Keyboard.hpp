bbf98555ea581ac15c80106cf862e7b0bac719c8
bomkei <93323570+bomkei@users.noreply.github.com>
2022-06-27 03:14:00 +0900

    [libctrpf::KeyboardImpl] _no_exit_with_enter が true であれば、エンターを押したときにイベントを発生させる

--- libctrpf/include/CTRPluginFramework/Menu/Keyboard.hpp
+++ libctrpf/include/CTRPluginFramework/Menu/Keyboard.hpp
@@ -17,6 +17,7 @@ namespace CTRPluginFramework
         {
             CharacterAdded,
             CharacterRemoved,
+            EnterPressed,
             InputWasCleared,
             SelectionChanged,
             KeyPressed,
