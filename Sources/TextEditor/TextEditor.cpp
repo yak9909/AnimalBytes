21a3fa9692028f9fa2e0cd7a33a51152324eb1fe
bomkei <93323570+bomkei@users.noreply.github.com>
2022-11-21 16:06:03 +0900

    久しぶりのコミット

--- Sources/TextEditor/TextEditor.cpp
+++ Sources/TextEditor/TextEditor.cpp
@@ -3,37 +3,42 @@
 #include "impl/TEImpl.h"
 
 namespace CTRPluginFramework {
-  static TextEditor* __g_instance;
+static TextEditor* __g_instance;
 
-  TextEditor::TextEditor()
+TextEditor::TextEditor()
     : impl(new TextEditorImpl)
-  {
-    __g_instance = this;
-  }
-
-  TextEditor::TextEditor(std::string const& path)
-    : impl(new TextEditorImpl(path))
-  {
-  }
-
-  TextEditor::~TextEditor()
-  {
-  }
-
-  void TextEditor::add_event(EventFuncPointer func) {
-    impl->event_funcs.emplace_back(func);
-  }
-
-  void TextEditor::add_submenu_entry(std::string const& name, EventFuncPointer func) {
-    impl->submenu_user_entries.emplace_back(name, func);
-  }
-
-  TextEditor::Result TextEditor::open() const {
-    return impl->open();
-  }
-
-  TextEditor* TextEditor::get_instance() {
-    return __g_instance;
-  }
-
+{
+  __g_instance = this;
 }
+
+TextEditor::TextEditor(std::string const& path)
+    : impl(new TextEditorImpl(path))
+{
+}
+
+TextEditor::~TextEditor()
+{
+}
+
+void TextEditor::add_event(EventFuncPointer func)
+{
+  impl->event_funcs.emplace_back(func);
+}
+
+void TextEditor::add_submenu_entry(std::string const& name,
+                                   EventFuncPointer func)
+{
+  impl->submenu_user_entries.emplace_back(name, func);
+}
+
+TextEditor::Result TextEditor::open() const
+{
+  return impl->open();
+}
+
+TextEditor* TextEditor::get_instance()
+{
+  return __g_instance;
+}
+
+}  // namespace CTRPluginFramework
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -8,66 +8,82 @@
 
 #include "TextEditor.h"
 
-static char const* ABOUT =
-  "Animal Bytes: ACNL Plugin\n"
-  "\n"
-  "Developers:\n"
-  "  bomkei (Main Developer)\n"
-  "  yak9909 (Cheat codes)\n"
-  "  HIDE810 (Debuggings)\n";
+static char const *ABOUT =
+    "Animal Bytes: ACNL Plugin\n"
+    "\n"
+    "Developers:\n"
+    "  bomkei (Main Developer)\n"
+    "  yak9909 (Cheat codes)\n"
+    "  HIDE810 (Debuggings)\n";
 
 /* ---------------------
 
 注意
   以下のアドレスは、AnimalBytes によって予約されているものです
-  Process::CheckAddress がこれらに対して false を返すようになっています
+  Process::CheckAddress がこれらに対して false
+を返すようになっています
 
   0x838000 ~ 0x839000
 
   --------------------- */
 
 namespace CTRPluginFramework {
-  void PatchProcess(FwkSettings &settings) {
-    // force drop
-    *(u32*)0x59E5DC = 0xEA000004;
+void PatchProcess(FwkSettings &settings)
+{
+  __attribute__((packed)) struct patch_table_t {
+    u32 addr;
+    u32 enable;
+    u32 disable;
+  };
 
+  static constexpr patch_table_t patchlist[] = {
+      // force drop
+      {0x59E5DC, 0xEA000004, 0x0A000004},
+  };
 
-  }
+  for (auto &&[A, E, D] : patchlist)
+    *(u32 *)A = E;
 
-  void OnProcessExit(void) {
-  }
-
-  void InitMenu(PluginMenu& menu) {
-    
-    Cheats::TextToCheats::initialize(menu);
-    Cheats::SpeedHacks::initialize();
-
-    Cheats::CodeContext::get_instance()->init();
-
-    menu += new MenuEntry("TextEditor", nullptr, [] (MenuEntry* e) {
-      static TextEditor editor;
-
-      editor.open();
-    });
-
-    menu += Cheats::Movements::make_folder();
-    menu += Cheats::SpeedHacks::make_folder();
-    menu += Cheats::Items::make_folder();
-    menu += Cheats::ItemDrops::make_folder();
-  }
-
-  int main() {
-    PluginMenu menu{ "AnimalBytes", 1, 0, 0, ABOUT };
-    Logger logger{ "plugin.log" };
-
-    menu.SynchronizeWithFrame(true);
-    menu.ShowWelcomeMessage(false);
-
-    // Create Menu
-    InitMenu(menu);
-
-    OSD::Notify(Color::DodgerBlue << "AnimalBytes Ready!!");
-
-    return menu.Run();
-  }
+  // force drop
+  // *(u32 *)0x59E5DC = 0xEA000004;
 }
+
+void OnProcessExit(void)
+{
+}
+
+void InitMenu(PluginMenu &menu)
+{
+  Cheats::TextToCheats::initialize(menu);
+  Cheats::SpeedHacks::initialize();
+
+  Cheats::CodeContext::get_instance()->init();
+
+  menu += new MenuEntry("TextEditor", nullptr, [](MenuEntry *e) {
+    static TextEditor editor;
+
+    editor.open();
+  });
+
+  menu += Cheats::Movements::make_folder();
+  menu += Cheats::SpeedHacks::make_folder();
+  menu += Cheats::Items::make_folder();
+  menu += Cheats::ItemDrops::make_folder();
+}
+
+int main()
+{
+  PluginMenu menu{"AnimalBytes", 1, 0, 0, ABOUT};
+  Logger logger{"plugin.log"};
+
+  menu.SynchronizeWithFrame(true);
+  menu.ShowWelcomeMessage(false);
+
+  // Create Menu
+  InitMenu(menu);
+
+  OSD::Notify(Color::DodgerBlue << "AnimalBytes Ready!!");
+
+  return menu.Run();
+}
+}  // namespace CTRPluginFramework
